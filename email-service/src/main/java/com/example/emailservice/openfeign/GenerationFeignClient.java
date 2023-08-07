a206a015e21e0251a9bf363433758c0e74e59dd5
shaheenshaikh3399 <112388992+shaheenshaikh3399@users.noreply.github.com>
2023-08-07 17:02:47 +0530

    Add files via upload
    Email service updated

--- email-service/src/main/java/com/example/emailservice/openfeign/GenerationFeignClient.java
+++ email-service/src/main/java/com/example/emailservice/openfeign/GenerationFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "GENERATION-SERVICE", path = "/api/policies")
+@FeignClient(name = "POLICY-GENERATION-SERVICE", path = "/policies/generate")
 public interface GenerationFeignClient {
     @GetMapping("/getById/{id}")
     ResponseEntity<PolicyDocumentDto> getPolicyDocumentById(@PathVariable("id") String documentId);
