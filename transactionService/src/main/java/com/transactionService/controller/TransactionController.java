45a04f8b7d44842fb7cccf869b77c36c7e30aaf9
shaheenshaikh3399 <112388992+shaheenshaikh3399@users.noreply.github.com>
2023-08-07 17:08:05 +0530

    Add files via upload
    Transaction service updated

--- transactionService/src/main/java/com/transactionService/controller/TransactionController.java
+++ transactionService/src/main/java/com/transactionService/controller/TransactionController.java
@@ -16,16 +16,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/payments")
 public class TransactionController {
     private final TransactionService transactionService;
-    @CircuitBreaker(name = "makePaymentCircuitBreaker", fallbackMethod = "makePaymentFallBack")
+    //@CircuitBreaker(name = "makePaymentCircuitBreaker", fallbackMethod = "makePaymentFallBack")
     @PostMapping("/{id}")
     public ResponseEntity<TransactionResponseDto> makePayment(@PathVariable ("id") String selectionId, @RequestBody Transaction transaction){
         return new ResponseEntity<>(transactionService.makePayment(selectionId, transaction), HttpStatus.OK);
     }
-    public ResponseEntity<TransactionResponseDto> makePaymentFallBack(String selectionId, Transaction transaction, Exception exception) {
+    /*public ResponseEntity<TransactionResponseDto> makePaymentFallBack(String selectionId, Transaction transaction, Exception exception) {
         return new ResponseEntity<>(TransactionResponseDto.builder()
                 .paymentStatus(PaymentStatus.PaymentGatewayDown)
                 .build(), HttpStatus.BAD_REQUEST);
-    }
+    }*/
     @GetMapping("/getById/{id}")
     public ResponseEntity<TransactionDto> getTransactionById(@PathVariable ("id") String transactionId){
         return new ResponseEntity<>(transactionService.getById(transactionId), HttpStatus.OK);
--- transactionService/src/main/java/com/transactionService/openfeign/SelectionFeignClient.java
+++ transactionService/src/main/java/com/transactionService/openfeign/SelectionFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "SELECTION-SERVICE", path = "/api/policies/select")
+@FeignClient(name = "POLICY-SELECTION-SERVICE", path = "/policies/select")
 public interface SelectionFeignClient {
     @GetMapping("/details/{id}")
     public ResponseEntity<PolicySelectionResponse> getSelectedDetailsById(@PathVariable("id") String policySelectionId);
